5b321341a9928bcb10a16b847b1855c8ccfaa8b6
Yonits12 <yonits12@gmail.com>
2020-02-03 14:29:59 +0200

    Sum of 2 nums

--- YoniCpp1/main.cpp
+++ YoniCpp1/main.cpp
@@ -5,8 +5,17 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
+    int num1 = 0, num2 = 0;
+    int sum;
+
+    cout << "Enter a number" << endl;
+    cin >> num1;
+
+    cout << "Enter another number" << endl;
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "The sum is: " << sum;
+
     return 0;
 }
